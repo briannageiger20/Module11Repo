4fe363f9c3565a05155cd893fc4ee63b7d0a1f34
briannageiger20 <brianna.geiger20@gmail.com>
2025-11-23 20:49:00 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 # Brianna Geiger
 # 11/23/2025
 # A simple python program to print a message
+
+# Brianna Geiger
+# 11/23/2025
+# Added new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
